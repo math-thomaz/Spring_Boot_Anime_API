f2d3fce10253bf0e5988866b2a63406faddac4f7
Matheus Thomaz <matheustthomaz@gmail.com>
2021-10-13 20:16:09 -0300

    Started project.

---
+++ src/main/java/com/maththomaz/animeapibydevdojo/AnimeapibydevdojoApplication.java
@@ -0,0 +1,13 @@
+package com.maththomaz.animeapibydevdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeapibydevdojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeapibydevdojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maththomaz/animeapibydevdojo/AnimeapibydevdojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maththomaz.animeapibydevdojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeapibydevdojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
